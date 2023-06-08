1625569aa3b8ee73ddfbc49c0d9005b81b65d96c
Nazar Hovar <121227077+nazarhovar@users.noreply.github.com>
2023-06-08 18:02:23 +0300

    Update CountryDao.java

--- src/main/java/Daos/CountryDao.java
+++ src/main/java/Daos/CountryDao.java
@@ -3,7 +3,7 @@ package Daos;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Set;
-
+//
 public interface CountryDao {
     Integer getCountryIdByName(String countryName, Connection connection) throws SQLException;
     void linkFilmWithCountries(int filmId, Set<String> countries, Connection connection) throws SQLException;
