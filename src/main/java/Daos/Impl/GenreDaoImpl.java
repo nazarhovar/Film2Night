41ab3deb151a004e325e55b6dc9559c3b6933c29
Nazar Hovar <nazar.govor@gmail.com>
2023-06-08 19:14:26 +0300

    update

--- src/main/java/Daos/Impl/GenreDaoImpl.java
+++ src/main/java/Daos/Impl/GenreDaoImpl.java
@@ -10,5 +10,52 @@ import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
-public class GenreDaoImpl {
+public class GenreDaoImpl implements GenreDao{
+
+    public Integer getGenreIdByName(String genreName, Connection connection) throws SQLException {
+        String query = "SELECT id FROM genre WHERE name = ?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, genreName);
+        ResultSet resultSet = statement.executeQuery();
+        if (resultSet.next()) {
+            return resultSet.getInt("id");
+        }
+        return null;
+    }
+
+    public void linkFilmWithGenres(int filmId, Set<String> genres, Connection connection) throws SQLException {
+        if (genres != null && !genres.isEmpty()) {
+            String query = "INSERT INTO film_genre (film_id, genre_id) VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            for (String genre : genres) {
+                String[] genreNames = genre.split(",");
+                for (String genreName : genreNames) {
+                    genreName = genreName.trim();
+                    Integer genreId = getGenreIdByName(genreName, connection);
+                    if (genreId != null) {
+                        statement.setInt(1, filmId);
+                        statement.setInt(2, genreId);
+                        statement.executeUpdate();
+                    }
+                }
+            }
+        }
+    }
+
+    public Set<String> getGenresByFilmId(int filmId) throws SQLException, ClassNotFoundException {
+        Set<String> genres = new HashSet<>();
+        Connection connection = ConnectorToDB.getConnection();
+        String query = "SELECT genre.name FROM genre " +
+                "JOIN film_genre ON film_genre.genre_id = genre.id " +
+                "WHERE film_genre.film_id = ?";
+
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1, filmId);
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            String genreName = resultSet.getString("name");
+            genres.add(genreName);
+        }
+        return genres;
+    }
 }
--- src/main/java/Load/Top250Loader.java
+++ src/main/java/Load/Top250Loader.java
@@ -14,6 +14,19 @@ public class Top250Loader extends Parser {
     private static final String API_URL = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS";
     private static final String API_KEY = "e6a7be3b-3fc1-4e73-a048-b4bb298621b8";
 
-    public static void loadTop (int page) throws IOException {
+    public static List<Top250> loadTop (int page) throws IOException {
+        URL url = new URL(API_URL + "&page=" + page);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("accept", "application/json");
+        connection.setRequestProperty("X-API-KEY",API_KEY);
+        connection.connect();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(connection.getInputStream());
+        List<Top250> top250 = Parser.parseTopFromJSON(jsonNode);
+        System.out.println(top250);
+        return top250;
     }
+
 }
